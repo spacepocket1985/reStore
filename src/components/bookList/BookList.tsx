4614a75e33f2283ab41150c18d815c6a791d3228
Space Pocket <space1985@pocket@gmail.com>
2024-02-13 20:56:43 +0300

    refactor: update mapDispatchToProps

--- src/components/bookList/BookList.tsx
+++ src/components/bookList/BookList.tsx
@@ -1,12 +1,11 @@
 import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
 import { useEffect } from 'react';
 import {
   booksRequestedAC,
   booksLoadedAC,
   booksErrorAC,
-  ActionBooksErrorType,
-  ActionBooksLoadedType,
-  ActionBooksRequestedType,
+
 } from '../../store/actions/actions';
 import { AppRootState } from '../../store/store';
 import { BookListItem } from '../bookListItem/BookListItem';
@@ -20,44 +19,43 @@ import { ErrorIndicator } from '../errorIndicator/ErrorIndicator';
 type MapStateToPropsType = {
   books: Array<BookType>;
   loading: boolean;
-  error: null | string
+  error: null | string;
 };
 
 type MapDispatchToPropsType = {
-  booksLoadedAC: (newBooks: Array<BookType>) => ActionBooksLoadedType;
-  booksRequestedAC: () => ActionBooksRequestedType;
-  booksErrorAC: (error: Error) => ActionBooksErrorType;
+  fetchBooks: () => void;
 };
 
 type BookListPropsType = MapStateToPropsType & MapDispatchToPropsType;
 
 const BookList = (props: BookListPropsType) => {
-  const { getBook } = bookStoreService();
-  const { books, loading, error, booksLoadedAC, booksRequestedAC, booksErrorAC } = props;
+  const {
+    books,
+    loading,
+    error,
+    fetchBooks
+  } = props;
 
   useEffect(() => {
-    booksRequestedAC();
-    getBook().then((data) => {
-      booksLoadedAC(data);
-    }).catch(booksErrorAC);
+    fetchBooks()
   }, []);
 
-  const renderBooks = (books: BookType []) => {
+  const renderBooks = (books: BookType[]) => {
     return (
       <ul className="book-list">
-      {books.map((book) => {
-        return (
-          <li key={book.id}>
-            <BookListItem book={book} />
-          </li>
-        );
-      })}
-    </ul>
-    )
-  }
+        {books.map((book) => {
+          return (
+            <li key={book.id}>
+              <BookListItem book={book} />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
 
   const spinner = loading ? <Spinner /> : null;
-  const isError = error? <ErrorIndicator errorMsg={error}/>: null
+  const isError = error ? <ErrorIndicator errorMsg={error} /> : null;
   const content = !(loading || isError) ? renderBooks(books) : null;
 
   return (
@@ -77,6 +75,20 @@ const mapStateToProps = (state: AppRootState): MapStateToPropsType => {
   };
 };
 
-const mapDispatchToProps = { booksLoadedAC, booksRequestedAC, booksErrorAC };
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+  const { getBook } = bookStoreService();
+
+  return {
+    fetchBooks: () => {
+      dispatch(booksRequestedAC());
+      getBook()
+        .then((data) => {
+          dispatch(booksLoadedAC(data));
+        })
+        .catch((error)=>{dispatch(booksErrorAC(error))});
+    },
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(BookList);
